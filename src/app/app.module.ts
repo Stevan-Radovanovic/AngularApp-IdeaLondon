d87409bc5b7148628217835c20c58ce44bb5be00
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-31 22:59:51 +0200

    Added bottom sheet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { HomeComponent } from './home/home.component';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    BottomSheetComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,6 +32,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatCardModule,
     HttpClientModule,
+    MatBottomSheetModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -33,6 +41,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
       },
     }),
   ],
+  entryComponents: [BottomSheetComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../product.model';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { BottomSheetComponent } from '../bottom-sheet/bottom-sheet.component';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +10,18 @@ import { Product } from '../product.model';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private serv: ProductService) {}
+  constructor(
+    private serv: ProductService,
+    private bottomSheet: MatBottomSheet
+  ) {}
 
   products: Product[] = [];
 
   ngOnInit(): void {
     this.products = this.serv.getProducts();
+  }
+
+  openBottomSheet() {
+    this.bottomSheet.open(BottomSheetComponent);
   }
 }
