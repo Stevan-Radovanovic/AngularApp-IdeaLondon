d7fafa38253144d46320dc53bf29eea2892b4c5c
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-31 22:23:30 +0200

    Added new products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -23,6 +23,31 @@ export class ProductService {
       subtitle: 'card.subtitle3',
       content: 'card.content3',
     },
+    {
+      title: 'card.title4',
+      subtitle: 'card.subtitle4',
+      content: 'card.content4',
+    },
+    {
+      title: 'card.title5',
+      subtitle: 'card.subtitle5',
+      content: 'card.content5',
+    },
+    {
+      title: 'card.title6',
+      subtitle: 'card.subtitle6',
+      content: 'card.content6',
+    },
+    {
+      title: 'card.title7',
+      subtitle: 'card.subtitle7',
+      content: 'card.content7',
+    },
+    {
+      title: 'card.title8',
+      subtitle: 'card.subtitle8',
+      content: 'card.content8',
+    },
   ];
 
   getProducts() {
