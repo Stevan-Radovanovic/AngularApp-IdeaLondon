549e456c123afb262fd5fe2e964fa0ae7e3c8fc9
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-05-31 17:31:30 +0200

    Added serbian translations and changing languages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private translate: TranslateService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  useLanguage(language: string) {
+    this.translate.use(language);
   }
-
 }
